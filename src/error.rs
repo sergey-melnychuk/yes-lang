28362ef6b9558b8e59f7e2cde7b66b6f4c19c844
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2021-08-01 15:53:59 +0200

    Add EvalError and Error union for application-specific errors

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,8 @@
 use crate::token::Token;
-use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::num::{ParseIntError, ParseFloatError};
+use crate::parser::Operator;
+use crate::eval::Object;
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -19,7 +20,7 @@ impl Display for TokenError {
     }
 }
 
-impl Error for TokenError {}
+impl std::error::Error for TokenError {}
 
 #[derive(Debug, Eq, PartialEq)]
 pub(crate) enum ParserError {
@@ -42,29 +43,39 @@ impl Display for ParserError {
     }
 }
 
-impl Error for ParserError {}
+impl std::error::Error for ParserError {}
 
 #[derive(Debug)]
 pub(crate) enum EvalError {
     ParseInt(ParseIntError),
     ParseFloat(ParseFloatError),
     NotFound(String),
+    InfixOp(Operator, Object, Object),
+    PrefixOp(Operator, Object),
     Other(String),
 }
 
 impl Display for EvalError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            EvalError::ParseInt(e) => write!(f, "Parsing integer failed: {}", e),
-            EvalError::ParseFloat(e) => write!(f, "Parsing float failed: {}", e),
-            EvalError::NotFound(name) => write!(f, "Not found: '{}'", name),
-            EvalError::Other(msg) => write!(f, "Evaluation failed: {}", msg)
+            EvalError::ParseInt(e) =>
+                write!(f, "Parsing integer failed: {}", e),
+            EvalError::ParseFloat(e) =>
+                write!(f, "Parsing float failed: {}", e),
+            EvalError::NotFound(name) =>
+                write!(f, "Not found: '{}'", name),
+            EvalError::InfixOp(op, lhs, rhs) =>
+                write!(f, "Cannot apply: '{:?}' {:?} '{:?}'", op, lhs, rhs),
+            EvalError::PrefixOp(op, obj) =>
+                write!(f, "Cannot apply: {:?} '{:?}'", op, obj),
+            EvalError::Other(msg) =>
+                write!(f, "Evaluation failed: {}", msg)
         }
     }
 }
 
-impl Error for EvalError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
+impl std::error::Error for EvalError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             EvalError::ParseInt(e) => Some(e),
             EvalError::ParseFloat(e) => Some(e),
@@ -82,5 +93,42 @@ impl From<ParseIntError> for EvalError {
 impl From<ParseFloatError> for EvalError {
     fn from(e: ParseFloatError) -> Self {
         Self::ParseFloat(e)
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum Error {
+    Token(TokenError),
+    Parser(ParserError),
+    Eval(EvalError),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Token(e) => write!(f, "{}", e),
+            Error::Parser(e) => write!(f, "{}", e),
+            Error::Eval(e) => write!(f, "{}", e),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<TokenError> for Error {
+    fn from(e: TokenError) -> Self {
+        Self::Token(e)
+    }
+}
+
+impl From<ParserError> for Error {
+    fn from(e: ParserError) -> Self {
+        Self::Parser(e)
+    }
+}
+
+impl From<EvalError> for Error {
+    fn from(e: EvalError) -> Self {
+        Self::Eval(e)
     }
 }
